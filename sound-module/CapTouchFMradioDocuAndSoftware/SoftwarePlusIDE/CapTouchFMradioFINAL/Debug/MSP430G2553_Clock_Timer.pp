eacbbdbd580881c7ef2335ea56ec72aa8592c0e3
Lenobra <Leonard.JRother@gmail.com>
2025-10-21 18:01:24 +0200

    sorted

--- sound-module/CapTouchFMradioDocuAndSoftware/SoftwarePlusIDE/CapTouchFMradioFINAL/Debug/MSP430G2553_Clock_Timer.pp
+++
@@ -1,9 +0,0 @@
-# FIXED
-
-MSP430G2553_Clock_Timer.obj: ../MSP430G2553_Clock_Timer.c
-MSP430G2553_Clock_Timer.obj: C:/ti/ccsv5/ccs_base/msp430/include/msp430g2553.h
-MSP430G2553_Clock_Timer.obj: C:/ti/ccsv5/ccs_base/msp430/include/in430.h
-
-../MSP430G2553_Clock_Timer.c: 
-C:/ti/ccsv5/ccs_base/msp430/include/msp430g2553.h: 
-C:/ti/ccsv5/ccs_base/msp430/include/in430.h: 
